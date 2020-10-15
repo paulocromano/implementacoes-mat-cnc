91e89111fd5c6e61e4b29ebdff40b4e78de19afe
Paulo Romano <paulo-romano_133@hotmail.com>
2020-10-15 10:50:24 -0300

    Criação tela e implementação inicial da Integração Numérica

--- src/app/calculo-numerico-computacional/integracao-numerica/integracao-numerica.component.ts
+++ src/app/calculo-numerico-computacional/integracao-numerica/integracao-numerica.component.ts
@@ -5,11 +5,64 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './integracao-numerica.component.html',
   styleUrls: ['./integracao-numerica.component.css']
 })
+
 export class IntegracaoNumericaComponent implements OnInit {
+
+  public limiteSuperior: number = 0;
+  public limiteInferior: number = 0;
+
+  public funcao: string;
+  public h: number;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public calcular(): void {
+    this.h = this.converterParaFloat((this.limiteSuperior - this.limiteInferior) / this.limiteSuperior);
+    let x = this.calcularValoresDeX();
+
+    this.verificarSeExisteDivisao();
+
   }
 
+  /**
+   * @description Método responsável por calcular os valores de X
+   * @returns number[] - Contendo os valores de X
+   */
+  private calcularValoresDeX(): number[] {
+    let x = new Array<number>();
+
+    x.push(this.h + this.limiteInferior);
+    console.log(x);
+
+    return x;
+  }
+
+  /**
+   * @description Método responsável por verificar se existe divisão
+   * @returns string[]
+   */
+  private verificarSeExisteDivisao(): string[] {
+    return this.funcao.split("/");
+  }
+
+  /**
+   * @description Método responsável por fixar duas casas decimais e converter de volta pra number
+   * @param valor : number
+   * @returns number - Valor convertido
+   */
+  private converterParaFloat(valor: number): number {
+    return parseFloat(valor.toFixed(2));
+  }
+
+  /**
+   * Resetar os valores da Integração
+   */
+  public resetarValores(): void {
+    this.limiteSuperior = 0;
+    this.limiteInferior = 0;
+    this.funcao = null;
+    this.h = null;
+  } 
 }
--- src/app/calculo-numerico-computacional/integracao-numerica/integracao-numerica.module.ts
+++ src/app/calculo-numerico-computacional/integracao-numerica/integracao-numerica.module.ts
@@ -1,15 +1,22 @@
-import { IntegracaoNumericaComponent } from './integracao-numerica.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import {DropdownModule} from 'primeng/dropdown';
+import {InputTextModule} from 'primeng/inputtext';
 
+import { IntegracaoNumericaComponent } from './integracao-numerica.component';
 
 @NgModule({
   declarations: [
     IntegracaoNumericaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    DropdownModule,
+    InputTextModule
   ]
 })
+
 export class IntegracaoNumericaModule { }
